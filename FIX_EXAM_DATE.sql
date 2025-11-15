b213653fb3918220a54773d38a685945992e2638
Caden <cadenchiang@gmail.com>
2025-11-14 18:33:04 -0800

    Fix SQL script - remove non-existent learning_paths table

--- FIX_EXAM_DATE.sql
+++ FIX_EXAM_DATE.sql
@@ -12,17 +12,9 @@ SET scheduled_date = '2025-12-11'
 WHERE item_type = 'exam_day'
 AND scheduled_date = '2025-12-12';
 
--- Update learning_paths table
-UPDATE learning_paths
-SET exam_date = '2025-12-11'
-WHERE exam_date = '2025-12-12';
-
 -- Verify the changes
 SELECT 'user_goals' as table_name, target_exam_date as exam_date
 FROM user_goals
-UNION ALL
-SELECT 'learning_paths', exam_date
-FROM learning_paths
 UNION ALL
 SELECT 'learning_path_items', scheduled_date
 FROM learning_path_items
