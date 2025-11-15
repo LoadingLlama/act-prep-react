a3fc9ebb9889f563d5c80366fc3c6f5f383f966d
Caden <cadenchiang@gmail.com>
2025-11-14 18:31:45 -0800

    Add SQL script to fix exam date from Dec 12 to Dec 11

---
+++ FIX_EXAM_DATE.sql
@@ -0,0 +1,29 @@
+-- Fix exam date from Dec 12 to Dec 11
+-- Run this in Supabase SQL Editor to correct the stored exam date
+
+-- Update user_goals table
+UPDATE user_goals
+SET target_exam_date = '2025-12-11'
+WHERE target_exam_date = '2025-12-12';
+
+-- Update learning_path_items table (exam_day items)
+UPDATE learning_path_items
+SET scheduled_date = '2025-12-11'
+WHERE item_type = 'exam_day'
+AND scheduled_date = '2025-12-12';
+
+-- Update learning_paths table
+UPDATE learning_paths
+SET exam_date = '2025-12-11'
+WHERE exam_date = '2025-12-12';
+
+-- Verify the changes
+SELECT 'user_goals' as table_name, target_exam_date as exam_date
+FROM user_goals
+UNION ALL
+SELECT 'learning_paths', exam_date
+FROM learning_paths
+UNION ALL
+SELECT 'learning_path_items', scheduled_date
+FROM learning_path_items
+WHERE item_type = 'exam_day';
